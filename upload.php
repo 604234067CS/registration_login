4baa6b155f2d92730ac75f1ca5af3d92feb9c6bd
604234067CS <604234067@parichat.skru.ac.th>
2020-12-22 17:39:59 +0700

    upload image

--- upload.php
+++ upload.php
@@ -1,7 +1,40 @@
 <?php 
 
 require_once('connection.php');
-if(!empty($_FILES)){ 
+
+$target_dir = "uploads/";
+$target_file = $target_dir.$_FILES['file_name']['name'];
+$msg = '';
+
+srand((double)microtime()*10000000);
+$str = 'abcdefghijklmnopqrstuvwxyz0123456789'; //string ที่เป็นไปได้ที่จะใช้ในการ random ซึ่งสามารถเพิ่มลดได้ตามความต้องการ
+$passw = str_shuffle($str);
+$str_result = "";  //สตริงว่างสำหรับจะรับค่าจากการ random
+$str_result .= substr($passw ,1,20); //ต่อ string จาก substring ที่ได้จากการ random ตำแหน่ง ทีละ 1 ตัว 
+                           //จนกว่าจะครบตรามความยาวที่ส่งมา
+
+         $file_name = explode(".", $_FILES['file_name']['name']);  
+         $allowed_ext = array("jpg", "jpeg", "png", "gif");  
+              $new_name = $str_result . '.' . $file_name[1];  
+              $sourcePath = $_FILES['file_name']['tmp_name'];  
+  //echo  $dir = $_GET['dir'];
+              $target_file = 'uploads/'.$new_name;  
+
+  if(move_uploaded_file($_FILES['file_name']['tmp_name'], $target_file)){
+   //$date_n = date('Y-m-d');
+   //$hour = date('H:i:s');
+   $sql = "INSERT INTO files (file_name, uploaded_on) VALUES ('".$fileName."', NOW())"; 
+   $insert = mysqli_query($conn, $sql);
+//mysql_query($insert);
+  }else{
+    $msg = 'Error while uploading.';
+  }
+
+echo $msg;
+exit;
+
+
+/*if(!empty($_FILES)){ 
   
      
     // การกำหนดค่าเส้นทางไฟล์
@@ -15,5 +48,10 @@ if(!empty($_FILES)){
         $sql = "INSERT INTO files (file_name, uploaded_on) VALUES ('".$fileName."', NOW())"; 
         $insert = $conn->query($sql);
     } 
-} 
+} */
+
+
+
 ?>
+
+    
--- upload_image.php
+++ upload_image.php
@@ -6,12 +6,21 @@ include_once('link.php');
     <div class="container">
         <h2>อัฟโหลดรูปภาพ</h2>
 
-        <div class="upload-div">
-            <form action="upload.php" class="dropzone" id="dropzone" method="POST"></form>
-        </div>
-        <div>
-        <button type="submit" name="UPLOAD" class="btn btn-primary">อัฟโหลด</button> 
-        </div>
+        <form action="include/upload_banner.php" id="frmTarget" class="dropzone" method="post" enctype="multipart/form-data">
+                                <div class="dz-message">
+                                    <div class="drag-icon-cph">
+                                        <i class="material-icons">touch_app</i>
+                                    </div>
+                                    <h3>Drop files here or click to upload.</h3>
+                                    <div class="fallback">
+										<input name="file" id="file" type="file" multiple />
+									</div>
+                                </div>
+                                <div>
+                                    <input type="submit" id="button" name="button" class="btn btn-primary" value="Upload">
+                                    <input type="button" class="btn bg-grey waves-effect" data-dismiss="modal" value="CLOSE" onClick="javascript:location.reload();">
+                                </div>
+                            </form>
 
     <script>
             Dropzone.autoDiscover = false;
@@ -21,8 +30,8 @@ include_once('link.php');
                     url: "upload.php",
                     paramName: "file_name",
                     maxFilesize: 2,
-                    maxFiles: 10,
-                    acceptedFiles: "image/*,application/pdf",
+                    parallelUploads : 15,
+                    acceptedFiles :".png,.jpg,.gif,.bmp,.jpeg",
                     autoProcessQueue: false
                 });
                 
@@ -31,6 +40,7 @@ include_once('link.php');
                 });
             });
         </script>
+
     </div>
 </body>
 
